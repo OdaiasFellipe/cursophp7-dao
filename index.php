585d25bb83329843cc6e3897925b5c2fd39f7839
unknown <Fernandes@HPROG03.hce.eb>
2022-10-19 14:02:44 -0300

    aprendemos a listar

--- index.php
+++ index.php
@@ -2,10 +2,29 @@
 
 require_once("config.php");
 
+/* retorna apenas o usuário do id
 $root = new Usuario();
-
 $root->loadbyId(3);
 
-echo $root;
+echo $root;*/
+
+
+//carrega uma lista de usuários
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//carrega uma lista pelo login
+
+/*$search = Usuario::search("ri");
+
+echo json_encode($search);*/
+
+//carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("odaias","sadafaf");
+
+echo $usuario;
 
 ?>
